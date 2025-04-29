1b5cef203ceb7b9b49da1101dab84c7bf8ebb38a
habibor-rahaman1010 <habibor144369@gmail.com>
2025-04-30 01:14:45 +0600

    validation implement

--- Medium/Medium.Web/Areas/Admin/Models/Category/CategoryCreatModel.cs
+++ Medium/Medium.Web/Areas/Admin/Models/Category/CategoryCreatModel.cs
@@ -1,9 +1,13 @@
-﻿namespace Medium.Web.Areas.Admin.Models.Category
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Medium.Web.Areas.Admin.Models.Category
 {
     public class CategoryCreatModel
     {
-        public string? Name { get; set; }
-        public string? Description { get; set; }
+        [Required(AllowEmptyStrings = false), StringLength(100)]
+        public string Name { get; set; } = string.Empty;
+        [Required(AllowEmptyStrings = false), StringLength(100)]
+        public string Description { get; set; } = string.Empty;
         public DateTime? CreatedDate { get; set; }
         public DateTime? UpdatedDate { get; set; }
     }
